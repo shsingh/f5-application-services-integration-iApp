39c04034e875d052b3c9486e533aeeec71177ae3
Krystian Marek <k.marek@f5.com>
2017-07-22 01:26:01 +0200

    update

--- tests/functional/test_2_BIP_scale.py
+++ tests/functional/test_2_BIP_scale.py
@@ -4,5 +4,5 @@ from src.appservices.TestTools import run_functional_tests_at_scale
 
 
 def test_functional_tests_at_scale(
-        get_config, bip_client, prepare_tests, scale_size):
-    run_functional_tests_at_scale(bip_client, get_config, scale_size)
+        get_config, bip_client, prepare_tests, get_scale_size):
+    run_functional_tests_at_scale(bip_client, get_config, get_scale_size)
