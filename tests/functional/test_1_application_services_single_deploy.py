09881c02fb9dab172a9683abfb15e6136bec443e
Krystian Marek <k.marek@f5.com>
2017-07-22 01:00:55 +0200

    update

--- tests/functional/test_1_application_services_single_deploy.py
+++ tests/functional/test_1_application_services_single_deploy.py
@@ -6,5 +6,5 @@ from src.appservices.TestTools import run_functional_tests
 
 
 @pytest.mark.skip(reason="Skipping in order to focus on scale")
-def functional_tests(get_config, bip_client, prepare_tests):
+def test_functional_tests(get_config, bip_client, prepare_tests):
     run_functional_tests(bip_client, get_config)
--- tests/functional/test_2_BIP_scale.py
+++ tests/functional/test_2_BIP_scale.py
@@ -3,6 +3,6 @@
 from src.appservices.TestTools import run_functional_tests_at_scale
 
 
-def functional_tests_at_scale(
+def test_functional_tests_at_scale(
         get_config, bip_client, prepare_tests, scale_size):
     run_functional_tests_at_scale(bip_client, get_config, scale_size)
