85848894e099dfc415a737113bd5e88fae987313
Krystian Marek <k.marek@f5.com>
2017-07-21 22:32:44 +0200

    skipping integration tests for ci testing

--- tests/functional/test_1_application_services_single_deploy.py
+++ tests/functional/test_1_application_services_single_deploy.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+import pytest
+
 from src.appservices.TestTools import run_functional_tests
 
 
+@pytest.mark.skip(reason="Skipping just for fun of it")
 def functional_tests(get_config, bip_client, prepare_tests):
     run_functional_tests(bip_client, get_config)
